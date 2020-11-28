86dfdb78ff8ed0d821da9b906e723b73e056b6c7
aldopolojr <aldopolojr@icloud.com>
2020-11-28 20:46:47 +0100

    version 1

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+const text = document.getElementById('text');
+let shadow = '';
+
+for (let i = 0; i < 500; i++){
+    shadow += (shadow ? ',' : '') + 1 * i + 'px ' + 1 * i + 'px 0 #01ded3';
+}
+
+text.style.textShadow = shadow;
